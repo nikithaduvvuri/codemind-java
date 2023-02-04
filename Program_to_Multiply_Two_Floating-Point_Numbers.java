13ac7db3c29360a3832b84dd9500f557928d7c1d
nikithaduvvuri <97437377+nikithaduvvuri@users.noreply.github.com>
2023-02-04 20:19:04 +0530

    import java.util.Scanner; class multiply {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         float a,b;         a=sc.nextFloat();         b=sc.nextFloat();         System.out.format("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class multiply
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float a,b;
+        a=sc.nextFloat();
+        b=sc.nextFloat();
+        System.out.format("%.2f",a*b);
+    }
+}
